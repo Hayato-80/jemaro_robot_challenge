b166ef85acf2574bb60748aa5d185aa02c750c4c
Hayato Kojima <140518562+Hayato-80@users.noreply.github.com>
2025-07-06 23:48:54 +0200

    Create install_galactic.sh

---
+++ install_galactic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install -y locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install -y curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y ros-galactic-desktop
+
+sudo apt install -y python3-colcon-common-extensions
+
+source /opt/ros/galactic/setup.bash
+echo "source /opt/ros/galactic/setup.bash" >> ~/.bashrc
+
+mkdir -p ~/galactic_ws/src
+cd ~/galactic_ws
+
+colcon build --symlink-install
+
+source install/setup.bash
